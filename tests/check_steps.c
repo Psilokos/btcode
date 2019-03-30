1452a14996e50645b1099d062a2ee9afe9161743
Victorien Le Couviour--Tuffet <victorien.lecouviour.tuffet@gmail.com>
2019-03-30 20:10:32 +0100

    tests: add DCT-IDCT check

--- tests/check_steps.c
+++ tests/check_steps.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include "dct.h"
+
+static int
+check_dct(void)
+{
+    int ret = BTCODE_SUCCESS;
+
+    int const n = rand() % 64 + 1;
+    int const n2 = n * n;
+
+    uint8_t *sm_in = NULL;
+    uint8_t *sm_out = NULL;
+    float *fm = NULL;
+
+    sm_in = malloc(n2); if (!sm_in) goto alloc_error;
+    sm_out = malloc(n2); if (!sm_out) goto alloc_error;
+    fm = malloc(n2 * sizeof(*fm)); if (!fm) goto alloc_error;
+
+    for (int i = 0; i < n2; ++i)
+        sm_in[i] = rand() % 256;
+
+    dct_forward(fm, sm_in, n);
+    dct_backward(sm_out, fm, n);
+
+    if (memcmp(sm_in, sm_out, n2))
+        goto test_failed;
+    goto ret;
+
+alloc_error:
+    ret = BTCODE_ERR(errno);
+    goto ret;
+test_failed:
+    ret = BTCODE_EGENERIC;
+ret:
+    free(sm_in);
+    free(sm_out);
+    free(fm);
+    return ret;
+}
 
 static struct
 {
@@ -9,6 +48,7 @@ static struct
 } const
 tests[] =
 {
+    { "DCT-IDCT", check_dct },
     {0}
 };
 
